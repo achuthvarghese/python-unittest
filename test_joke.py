08961f6938048a6ef376b60ecefdc955cab2241f
achuthvarghese <acvknr@gmail.com>
2022-05-15 13:01:33 +0530

    Bug fix: ModuleNotFoundError: No module named 'main'

--- test_joke.py
+++ test_joke.py
@@ -5,13 +5,13 @@ from joke import get_joke, len_joke
 
 
 class TestJoke(unittest.TestCase):
-    @patch("main.get_joke")
+    @patch("joke.get_joke")
     def test_len_joke(self, mock_get_joke):
         mock_get_joke.return_value = "some random joke"
 
         self.assertEqual(len_joke(), 16)
 
-    @patch("main.requests")
+    @patch("joke.requests")
     def test_get_joke(self, mock_requests):
         mock_response = MagicMock(status_code=200)
         mock_response.json.return_value = {"value": {"joke": "some other random joke"}}
@@ -20,7 +20,7 @@ class TestJoke(unittest.TestCase):
 
         self.assertEqual(get_joke(), "some other random joke")
 
-    @patch("main.requests")
+    @patch("joke.requests")
     def test_fail_get_joke(self, mock_requests):
         mock_response = MagicMock(status_code=403)
 
